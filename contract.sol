2fb358189c8db48a379353407d71de3faaf1fd6e
Nishant Pacharne <71060551+NishantPacharne@users.noreply.github.com>
2023-03-19 11:39:58 +0530

    created contract.sol

---
+++ contract.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract YourCollectionName is  ERC721Enumerable, Pausable, Ownable {
+    using Counters for Counters.Counter;
+    using Strings for uint256;
+
+    Counters.Counter private _tokenIdCounter;
+
+    uint256 public MINT_PRICE = 49 ether;
+    string public baseExtension = ".json";
+    uint256 public maxSupply = 5000;
+
+
+    constructor() ERC721("YourCollectionName", "YOURSYMBOL") {
+        _tokenIdCounter.increment();
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://bafybeib6k2drj6me247gemy5wouaxs2hqusn52dnnkgz22smaqrwyg33h4/";
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+
+    function mint(address to, uint256 amount) public payable {
+        require(!paused(), "Contract is paused, please try again later");
+        require(amount > 0);
+        uint256 supply = totalSupply();
+        require(supply + amount <= maxSupply);
+        require(msg.value >= MINT_PRICE * amount);
+
+
+        for (uint256 i = 1; i <= amount; i++) {
+            _safeMint(to, supply + i);
+        }
+    }
+
+    function withdraw() public onlyOwner() {
+        require(address(this).balance > 0, "Balance is zero");
+        payable(owner()).transfer(address(this).balance);
+    }
+
+
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        string memory currentBaseURI = _baseURI();
+        return
+            bytes(currentBaseURI).length > 0
+                ? string(
+                    abi.encodePacked(
+                        currentBaseURI,
+                        tokenId.toString(),
+                        baseExtension
+                    )
+                )
+                : "";
+    }
+}
